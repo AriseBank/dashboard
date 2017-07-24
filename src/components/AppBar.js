4710a985d58760f72843a9abf2b2288c3c5a7525
AriseBank <jared@dotoji.com>
2017-07-24 04:02:47 -0500

    Update AppBar.js

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -7,11 +7,11 @@ export default class AppBar extends React.Component {
       <div className="mui-appbar">
         <div className="left">
           <div className="back"><a href="https://www.stellar.org">&laquo; Stellar.org</a></div>
-          <div className="mui--text-headline">Stellar.org Dashboard</div>
+          <div className="mui--text-headline">Stellar Network Powered By AriseBank</div>
         </div>
         <div className="icons">
           <div className="icon">
-            <a href="https://github.com/stellar/dashboard" target="_blank">
+            <a href="https://github.com/arisebank/stellar-dashboard/" target="_blank">
               <i className="material-icons">code</i><br />
               GitHub
             </a>
